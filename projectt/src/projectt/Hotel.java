3b17e9de1559bcb3f61c207cada673fa4b3f4298
Advice <Advice@Advice.mshome.net>
2018-04-21 13:50:12 +0700

    master

--- projectt/src/projectt/Hotel.java
+++ projectt/src/projectt/Hotel.java
@@ -33,22 +33,19 @@ abstract class Hotel {
         Scanner a =new Scanner(System.in);
         System.out.print("Received :         ");
          h =a.nextInt();
-         
-        
          j=h-this.total ;
         System.out.println("Change :           " + (j+this.turn));
         System.out.println("Call : 061-2044434");
         System.out.println("Wifi : ketmaneenichakorn");
         
         
-         
-        
-    }
+         }
     
     public void billMonthly(int m){
         this.m = m;
         this.total = this.m*this.sum;
         this.turn = this.m*this.gM;
+        
         int j ,h ;
         System.out.println("***************Bill***************");
         System.out.println("Price :            " + this.total );
